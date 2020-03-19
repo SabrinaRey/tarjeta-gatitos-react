6c3e10237ffdf11fac33349aee3f354a5637703e
/SabrinaRey <sabrinareynares@gmail.com>
2020-03-18 21:57:53 -0300

    agregue styled-components

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import styled from 'styled-components'
+
+const ButtonComp = styled.button `
+background-color: #6D2451;
+color: #D1ABA7;
+box-shadow: 0 0 10px;
+`
+
+
 
 
 const Button = ({mensaje, estilo}) =>{
     return (
-    <button className={estilo}>{mensaje}</button>
+    <ButtonComp className={estilo}>{mensaje}</ButtonComp>
     )
 };
 
--- src/components/TarjetaDeGatos.js
+++ src/components/TarjetaDeGatos.js
@@ -1,18 +1,27 @@
 import React from 'react';
 import Button from './Button'
+import styled from 'styled-components'
 
+const Div = styled.div `
+.tarjetita {
+  margin-top: 20px;
+  width: 100%;
+  height: auto;
+  background-color: ${prop => prop.isAvailable ? '#6D2451' : 'rgb(95, 28, 69)'} 
+}
 
+`
 
 
 const TarjetaDeGatos = ({name, shortDesc, img, isAvailable}) => {  
   
   return (
-    <div className ={isAvailable ? "tarjetita" : "tarjetitaDisabled"}>
+    <Div isAvailable = {isAvailable}>
       <img src={img} alt={name} />
       <h3>{name}</h3>
       <p>{shortDesc}</p>
       <Button mensaje={isAvailable ? "Ver más": "No Disponible"} estilo = {"boton-vermas"}/>
-    </div>
+    </Div>
   )
 
 }
